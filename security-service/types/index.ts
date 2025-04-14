bada1341e947cad062d548def754b9f0d7f8af0b
monokkay kolie <egormayer08@mail.ru>
2025-04-14 18:32:52 +0200

    Update index.ts

--- security-service/types/index.ts
+++ security-service/types/index.ts
@@ -1,6 +1,5 @@
 import { Request } from 'express';
 
-// Define a user type with required properties
 export interface AuthUser {
   id: string;
   role?: string;
@@ -9,7 +8,6 @@ export interface AuthUser {
   [key: string]: any;
 }
 
-// Define a custom interface for request with user
 export interface RequestWithUser extends Request {
   user?: AuthUser;
 } 
